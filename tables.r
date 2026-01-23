4d4f32722e9b3b81ef9c2e85ab074f1a4ec86622
cusisom <danielcu@hawaii.edu>
2026-01-22 14:02:18 -1000

    dunno

---
+++ tables.r
@@ -0,0 +1,20 @@
+# Code for importing tables
+# I'm hoping that this will make for easy access
+#Working directory setwd("C:/Users/danny/Documents/git/Research_Methods/Analysis")
+
+## ---- Loadpackages --------
+
+require(geomorph) #for GM analysis
+require(SlicerMorphR) #for importing Slicer data
+require(plyr) #for wireframe specs
+require(dplyr) #for data processing/cleaning
+require(tidyr) #for data processing/cleaning
+require(skimr) #for nice visualization of data 
+require(knitr) #for qmd building
+require(gapminder) #for plot aesthetics
+
+## ---- loaddata -------
+
+data_path <- "../docs/"
+dictionary1 <- read.csv(paste(data_path, "CSample.csv", sep=""))
+knitr::kable(dictionary1)
