895559bcf69e7af05b6212abc16663fa29866952
cusisom <danielcu@hawaii.edu>
2026-01-26 12:24:15 -1000

    uh

--- tables.r
+++ tables.r
@@ -1,6 +1,6 @@
 # Code for importing tables
 # I'm hoping that this will make for easy access
-#Working directory setwd("C:/Users/danny/Documents/git/Research_Methods/Analysis")
+#Working directory setwd("C:/Users/danny/Documents/git/Research_Methods")
 
 ## ---- Loadpackages --------
 
@@ -13,9 +13,6 @@ require(skimr) #for nice visualization of data
 require(knitr) #for qmd building
 require(gapminder) #for plot aesthetics
 
-## ---- loaddata --------
-
-data_path <- "C:/Users/danny/Documents/git/Research_Methods/docs/"
 
 ## ---- Table1 --------
 dictionary1 <- read.csv(print("C:/Users/danny/Documents/git/Research_Methods/docs/Book1.csv"))
