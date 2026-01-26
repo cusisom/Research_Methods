fedc6c95c5953aae06ddd9b2a433477972d13925
cusisom <danielcu@hawaii.edu>
2026-01-26 11:49:42 -1000

    lost

--- tables.r
+++ tables.r
@@ -19,7 +19,7 @@ data_path <- "C:/Users/danny/Documents/git/Research_Methods/docs/"
 
 ## ---- Table1 --------
 Table1 <- read.csv(print("C:/Users/danny/Documents/git/Research_Methods/docs/Table1.csv"))
-knitr::kable(Table1)
+
 
 
 
